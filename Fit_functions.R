2c329024626a415ca4471f35ba53a155668db830
Lukas <mail@heiland.earth>
2021-11-20 21:25:12 +0100

    fix

--- Fit_functions.R
+++ Fit_functions.R
@@ -768,7 +768,7 @@ plotSensitivity <- function(fit, include, measure = "cjs_dist") {
     tools::file_path_sans_ext() %>%
     str_replace("-[1-9]-", "-x-")
   
-  ggsave(paste0("Fits.nosync/", basename, "_", "sens_powerscale", ".pdf"), densplot)
+  ggsave(paste0("Fits.nosync/", basename, "_", "sens_powerscale", ".pdf"), plot_powerscale)
   
   return(plot_powerscale)
 }
