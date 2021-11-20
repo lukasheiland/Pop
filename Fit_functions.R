a6dee0aa29051b28e3c4113f0cf234e76a23d348
Lukas <mail@heiland.earth>
2021-11-20 12:40:58 +0100

    Fix densplots

--- Fit_functions.R
+++ Fit_functions.R
@@ -686,9 +686,7 @@ plotDensCheck <- function(cmdstanfit, data_stan_priors, draws = NULL, check = c(
   
   plotname <- paste(names(densplots), check, sep = "_")
   
-  ggsave(paste0("Fits.nosync/", basename, "_", name, ".pdf"), densplot)
-  
-  mapply(function(p, n) ggsave(paste0("Fits.nosync/", basename, "_", n, ".pdf"), p), plots, plotname)
+  mapply(function(p, n) ggsave(paste0("Fits.nosync/", basename, "_", n, ".pdf"), p), densplots, plotname)
   ## cowplot::plot_grid(densplot, fixdensplot, labels = c("States", "Equilibria"), ncol = 1) #  axis = "b", align = "h"
   
   return(densplots)
