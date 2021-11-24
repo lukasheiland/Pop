1655762f9925cf9825f75a0cae390b482b2248db
Lukas <lukas.heiland@gmail.com>
2021-11-24 14:01:29 +0100

    weakpriors

--- _targets.R
+++ _targets.R
@@ -52,17 +52,18 @@ targets_settings <- list(
   tar_target(taxon_select, c("Fagus.sylvatica")),
   
   ## Weakly informative priors.
+  ## If provided, they are prioritzed over the fitted priors
   tar_target(weakpriors,
              ## Priors are organized like the parameter data structure but with an additional dimension in the case of a vector row of sds.
              list(
-               prior_b_log = c(-2, 2),
-               prior_c_a_log = c(-5, 2),
-               prior_c_b_log = c(-5, 2),
-               prior_c_j_log = c(-6, 2),
+               prior_b_log = c(-5, 2),
+               prior_c_a_log = c(-6, 2),
+               prior_c_b_log = c(-6, 2),
+               prior_c_j_log = c(-7, 3),
                ## prior_g_log,
                ## prior_h_log,
                prior_l_log = cbind(Fagus = c(0, 2), others = c(0, 2)),
-               # prior_r_log = cbind(Fagus = c(0.5, 2), others = c(0.5, 2)),
+               prior_r_log = cbind(Fagus = c(0, 2), others = c(0, 2)),
                prior_s_log = c(-2, 2)
              )
   )
