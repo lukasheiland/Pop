2ea7085609b84885f74aa8b96cd6201b8bdfb1ca
Lukas <lukas.heiland@gmail.com>
2021-12-11 12:33:48 +0100

    scripting

---
+++ _upload.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# scp ~/Documents/Studium/Projects/Pop/Data/Stages_s.rds ssh lukasheiland@rhsbio7.uni-regensburg.de:/home/lukasheiland/Projects/Pop/Data
+
+scp -r ~/Documents/Studium/Projects/Pop/ ssh lukasheiland@rhsbio7.uni-regensburg.de:/home/lukasheiland/Projects/
