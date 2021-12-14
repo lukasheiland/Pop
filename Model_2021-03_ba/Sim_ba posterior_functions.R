b3799ac7b27a0bc9ea8b7c5e09fbd064432c4f61
Lukas <lukas.heiland@gmail.com>
2021-12-14 21:55:43 +0100

    Fix posterior simulations
    from pars$ba_a_avg etc. NULL was added

--- Model_2021-03_ba/Sim_ba posterior_functions.R
+++ Model_2021-03_ba/Sim_ba posterior_functions.R
@@ -2,87 +2,86 @@
 # Posterior simulations ------------------------------------------------------------
 # ————————————————————————————————————————————————————————————————————————————————— #
 
-## iterateModel --------------------------------
-# cmdstanfit  <- tar_read("fit_test")
-# data_stan_priors <- tar_read("data_stan_priors")
-iterateModel <- function(initialstate,
-                         pars,
-                         times,
-                         format = c("long", "matrix")) {
-  
-  b <- pars$b # Length n_species vector of basal area increment rates.
-  c_a <- pars$c_a # Length n_species vector ## only used in model versions, where there are distinct parameters for competition on b an a
-  c_b <- pars$c_b # Length n_species vector
-  c_j <- pars$c_j # Length n_species vector
-  g <- pars$g # Length n_species vector of transition rates.
-  h <- pars$h # Length n_species vector of transition rates.
-  l <- pars$l # Length n_species vector of input rates.
-  r <- pars$r # Length n_species vector of input rates
-  s <- pars$s # Length n_species vector of shading rates
-  
-  ba_a_upper <- pars$ba_a_upper
-  ba_a_avg <- pars$ba_a_avg
-  
-  ## Set the count variables
-  n <- length(r) # no. of species
-  
-  ## 
-  times_intern <- 1:max(times)
-  n_times <- length(times_intern)
-  
-  # Prepare a state matrix
-  whichstate <- rep(1:3, each = n)
-  State <- matrix(rep(initialstate, times = n_times), nrow = n_times, byrow = T)
-  
-  ## Here comes the model.
-  for (t in 2:n_times) {
-    
-    ## States at t-1: *State*, and *State*
-    J <- State[t-1,whichstate == 1]
-    A <- State[t-1,whichstate == 2]
-    B <- State[t-1,whichstate == 3]
-    
-    ## The total basal area of big trees
-    BA <- A * ba_a_avg + B
-    BA_sum <- sum(BA)
-    
-    J_trans <- r*BA + l + (J - g*J) # use rlnorm + u[ ,1]
-    J_t <- J_trans * 1/(1 + c_j*sum(J) + s*BA_sum) # count of juveniles J
-    # with all variables > 0, and 0 < g, m_j < 1
-    
-    A_trans <- g*J + (A - h*A) # + u[ ,2]
-    A_t <-  A_trans * 1/(1 + c_a*BA_sum) # count of small adults
-    # with all variables > 0, and 0 < h, m_a < 1
-    
-    A_ba <- A * h * ba_a_upper # Basal area of small adults A. Conversion by multiplication with basal area of State exit (based on upper dhh boundary of the class)
-    B_trans <- A_ba + B # + u[ ,3]
-    B_t <- (1+b)*B_trans * 1/(1 + c_b*BA_sum)  # basal area of big adults B
-    ## b is the net basal area increment (including density-independent m) basically equivalent to a Ricker model, i.e. constant increment rate leading to exponential growth, negative density dependent limitation scaled with total BA_sum.
-    
-    State[t, ] <- c(J_t, A_t, B_t)
-  }
-  
-  whichtimes <- which(times_intern %in% times)
-  State <- State[whichtimes,]
-  
-  if(match.arg(format) == "long") State <- data.frame(abundance = c(State),
-                                                      stage = rep(c("j", "a", "b"), each = n * length(times)),
-                                                      tax = rep(1:n, each = length(times)),
-                                                      time = times)
-  
-  return(State)
-}
-
-
 ## simulateTrajectories --------------------------------
-# cmdstanfit <- tar_read("fit_test_pq")
+# cmdstanfit <- tar_read("fit_test")
 # parname <- tar_read("parname")
 # data_stan_priors <- tar_read("data_stan_priors")
 
 simulateTrajectories <- function(cmdstanfit, data_stan_priors, parname, time = seq(1, 5001, by = 100), thinstep = 1, usemean = FALSE) {
   
-  parname <- setdiff(parname, c("phi_obs", "sigma_l"))
+  parname <- setdiff(parname, c("phi_obs", "sigma_l", "sigma_k_loc"))
   parname_sans_log <- gsub("_log$", "", parname)
+  
+  
+  ## iterateModel --------------------------------
+  #
+  iterateModel <- function(initialstate,
+                           pars,
+                           times,
+                           format = c("long", "matrix")) {
+    
+    b <- pars$b # Length n_species vector of basal area increment rates.
+    c_a <- pars$c_a # Length n_species vector ## only used in model versions, where there are distinct parameters for competition on b an a
+    c_b <- pars$c_b # Length n_species vector
+    c_j <- pars$c_j # Length n_species vector
+    g <- pars$g # Length n_species vector of transition rates.
+    h <- pars$h # Length n_species vector of transition rates.
+    l <- pars$l # Length n_species vector of input rates.
+    r <- pars$r # Length n_species vector of input rates
+    s <- pars$s # Length n_species vector of shading rates
+    
+    ba_a_upper <- data_stan_priors$ba_a_upper
+    ba_a_avg <- data_stan_priors$ba_a_avg
+    
+    ## Set the count variables
+    n <- length(r) # no. of species
+    
+    times_intern <- 1:max(times)
+    n_times <- length(times_intern)
+    
+    # Prepare a state matrix
+    whichstate <- rep(1:3, each = n)
+    State <- matrix(rep(initialstate, times = n_times), nrow = n_times, byrow = T)
+    
+    ## Here comes the model.
+    for (t in 2:n_times) {
+      
+      ## States at t-1: *State*, and *State*
+      J <- State[t-1,whichstate == 1]
+      A <- State[t-1,whichstate == 2]
+      B <- State[t-1,whichstate == 3]
+      
+      ## The total basal area of big trees
+      BA <- A * ba_a_avg + B
+      BA_sum <- sum(BA)
+      
+      J_trans <- r*BA + l + (J - g*J) # use rlnorm + u[ ,1]
+      J_t <- J_trans * 1/(1 + c_j*sum(J) + s*BA_sum) # count of juveniles J
+      # with all variables > 0, and 0 < g, m_j < 1
+      
+      A_trans <- g*J + (A - h*A) # + u[ ,2]
+      A_t <-  A_trans * 1/(1 + c_a*BA_sum) # count of small adults
+      # with all variables > 0, and 0 < h, m_a < 1
+      
+      A_ba <- A * h * ba_a_upper # Basal area of small adults A. Conversion by multiplication with basal area of State exit (based on upper dhh boundary of the class)
+      B_trans <- A_ba + B # + u[ ,3]
+      B_t <- (1+b)*B_trans * 1/(1 + c_b*BA_sum)  # basal area of big adults B
+      ## b is the net basal area increment (including density-independent m) basically equivalent to a Ricker model, i.e. constant increment rate leading to exponential growth, negative density dependent limitation scaled with total BA_sum.
+      
+      State[t, ] <- c(J_t, A_t, B_t)
+    }
+    
+    whichtimes <- which(times_intern %in% times)
+    State <- State[whichtimes,]
+    
+    if(match.arg(format) == "long") State <- data.frame(abundance = c(State),
+                                                        stage = rep(c("j", "a", "b"), each = n * length(times)),
+                                                        tax = rep(1:n, each = length(times)),
+                                                        time = times)
+    
+    return(State)
+  }
+  
   
   Draws <- cmdstanfit$draws(variables = c(parname, "state_init_log", "L_loc"), format = "draws_list") %>%
     thin_draws(thin = thinstep)
@@ -120,13 +119,13 @@ simulateTrajectories <- function(cmdstanfit, data_stan_priors, parname, time = s
   ## Nested simulations: locs/draws
   
   iterateModel_draws <- function(state_init, l_loc, pars, time, usemean) {
-    S <- if(usemean) mean(state_init) else as_draws_matrix(state_init)
+    S_i <- if(usemean) mean(state_init) else as_draws_matrix(state_init)
     
     ## Assign local parameters to draws
-    L <- if(usemean) mean(l_loc) else as_draws_matrix(l_loc)
-    pars <- lapply(1:length(pars), function(i) within(pars[[i]], l <- c(L[i,])))
+    L_l <- if(usemean) mean(l_loc) else as_draws_matrix(l_loc)
+    pars <- lapply(1:length(pars), function(i) within(pars[[i]], l <- c(L_l[i,])))
     
-    return( lapply(1:nrow(S), function(i) iterateModel(c(S[i,]), pars = pars[[i]], time)) )
+    return( lapply(1:nrow(S_i), function(i) iterateModel(c(S_i[i,]), pars = pars[[i]], time)) )
   }
   
   iterateLocs <- function(i, S, L, p, t, um) {
@@ -135,10 +134,10 @@ simulateTrajectories <- function(cmdstanfit, data_stan_priors, parname, time = s
   
   # State_init <- State_init[10:12,]
   # L_loc <- L_loc[10:12,]
-  # time <- 1:5
+  # time <- 1:1000
   
-  sims <- future.apply::future_sapply(1:nrow(L_loc), iterateLocs,
-                                      S = State_init, L = L_loc, p = pars, t = time, um = usemean, simplify = F) # a nested list[locs, draws] of matrices[times]
+  sims <- future_sapply(1:nrow(L_loc), iterateLocs,
+                        S = State_init, L = L_loc, p = pars, t = time, um = usemean, simplify = F) # a nested list[locs, draws] of matrices[times]
   sims <- lapply(sims, bind_rows, .id = "draw")
   sims <- bind_rows(sims, .id = "loc")
   
