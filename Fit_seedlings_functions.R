56f3a8686fcb2eb315bca9e44f880dc19724cbaa
Lukas <lukas.heiland@gmail.com>
2021-12-21 20:51:31 +0100

    Revert "Revert "Fix L_loc""
    This reverts commit 09e6da1d26e01d18c045afc6981286cf3951731b.

--- Fit_seedlings_functions.R
+++ Fit_seedlings_functions.R
@@ -172,27 +172,39 @@ fitSeedlings <- function(Seedlings_s, fitpath) {
 
   fitModel <- function(tax) {
     
-   S  <- Seedlings_s[Seedlings_s$tax == tax,] %>% st_drop_geometry()
+   S <- Seedlings_s[Seedlings_s$tax == tax,] %>%
+     filter(ba_ha != 0 & ba_ha_sum != 0) %>%
+     st_drop_geometry()
     
     data_seedlings <- list(
       N = nrow(S),
       # N_locs = length(unique(S$plotid)),
       # rep_loc = as.integer(as.factor(S$plotid)), ## These are all the same anyway!
       y = round(as.integer(S$count_ha)),
+      ba_sum_log = log(S$ba_ha_sum),
+      ba_sum = S$ba_ha_sum,
       l_smooth_log = S[, paste0("s_", tax), drop = T], ## predictions are on the log scale!
-      ba_log = log(S$ba_ha) ## predictions are on the log scale!
+      ba_log = log(S$ba_ha),
+      ba = S$ba_ha
     )
     
-    fit_seedlings <- model_seedlings$sample(data = data_seedlings, parallel_chains = getOption("mc.cores", 4))
+    fit_Seedlings <- model_seedlings$sample(data = data_seedlings, parallel_chains = getOption("mc.cores", 4), output_dir = fitpath)
     
-    var <- c("k_log", "l_log", "r_log", "phi")
+    attr(fit_Seedlings, "data") <- data_seedlings
+    attr(fit_Seedlings, "taxon") <- tax
+    
+    var <- c("k_log", "l_log", "r_log", "s_log", "phi")
     
     ggsave(file.path(fitpath, paste0("Pairs_Seedlings_", tax, ".png")),
-           mcmc_pairs(fit_seedlings$draws(variables = var)))
-    message("Summary of the the fit for ", tax, ":")
-    print(fit_seedlings$summary(variables = var))
+           mcmc_pairs(fit_Seedlings$draws(variables = var)))
     
-    return(fit_seedlings)
+    s <- fit_Seedlings$summary(variables = var)
+    write.csv(s, file.path(fitpath, paste0("Summary_Seedlings_", tax, ".csv")))
+    
+    message("Summary of the the fit for ", tax, ":")
+    print(s)
+    
+    return(fit_Seedlings)
   }
   
 
@@ -204,6 +216,7 @@ fitSeedlings <- function(Seedlings_s, fitpath) {
       array[N] int y;
       vector[N] l_smooth_log;
       vector[N] ba_log;
+      vector[N] ba_sum_log;
     }
     
     parameters {
@@ -212,41 +225,72 @@ fitSeedlings <- function(Seedlings_s, fitpath) {
       // vector[N_locs] k_loc_log_raw;
       real l_log;
       real r_log;
+      real s_log;
       real<lower=0> phi_inv_sqrt;
     }
     
     transformed parameters {
       real<lower=0> phi = inv_square(phi_inv_sqrt);
-      vector<lower=0>[N] y_hat = exp(k_log) + // + k_loc_log_raw[rep_loc] * sigma_k_loc
-                                 exp(l_log + l_smooth_log) +
-                                 exp(r_log + ba_log); // exp(log(a) + log(b)) + exp(log(b) + log(c)) == a*b + b*c
+      vector<lower=0>[N] y_hat = exp(k_log - (s_log + ba_sum_log)) + // + k_loc_log_raw[rep_loc] * sigma_k_loc
+                                 exp(l_log + l_smooth_log) + // exp(log(a) + log(b)) + exp(log(b) + log(c)) == a*b + b*c
+                                 exp(r_log + ba_log);
+                                 
+                                 // prior inference model: seedlings ~ k/(s * ba_sum) + l * smooth + r * smooth; as we want to infer the seedling inputdue to ba and r
     }
     
     model {
       // priors
-      target += normal_lpdf(phi_inv_sqrt | 0, 1);
+      target += normal_lpdf(phi_inv_sqrt | 0, 0.01);
       // target += std_normal_lpdf(sigma_k_loc);
       // target += std_normal_lpdf(k_loc_log_raw);
-      target += normal_lpdf(k_log | 3, 2);
-      target += normal_lpdf(l_log | 0, 2);
-      target += normal_lpdf(r_log | 1, 2);
+      target += normal_lpdf(k_log | 0, 3);
+      target += normal_lpdf(l_log | 0, 3);
+      target += normal_lpdf(r_log | 0, 3);
+      target += normal_lpdf(s_log | 0, 3);
       
       // likelihood
       target += neg_binomial_2_lpmf(y | y_hat, phi);
     }
+    
+    generated quantities {
+      array[N] int y_sim = neg_binomial_2_rng(y_hat, phi);
+    }
   "
+  
   model_seedlings <- cmdstanr::cmdstan_model(stan_file = write_stan_file(code_seedlings))
   
-  # fit_seedlings <- brms::brm(count_ha ~ ba_ha + s_Fagus.sylvatica + 1 + (1 | plotid), # + offset(log(ba_ha_sum_p1_inv)), # + (1 | plotid),
+  # fit_Seedlings <- brms::brm(count_ha ~ ba_ha + s_Fagus.sylvatica + 1 + (1 | plotid), # + offset(log(ba_ha_sum_p1_inv)), # + (1 | plotid),
   #                            family = negbinomial,
   #                            prior = set_prior("cauchy(0,10)", class = "sd", group = "plotid"),
   #                            data = Seedlings_s[Seedlings_s$tax == "Fagus.sylvatica",],
   #                            cores = getOption("mc.cores", 4))
   
   
-  fits_seedlings <- lapply(c("Fagus.sylvatica", "other"), fitModel)
+  fits_Seedlings <- sapply(c("Fagus.sylvatica", "other"), fitModel, USE.NAMES = T, simplify = F)
   
+  
+  generateResiduals <- function(taxon) {
+    
+    fit_Seedlings <- fits_Seedlings[[taxon]]
+    data_seedlings <- attr(fit_Seedlings, "data")
+    
+    y_sim <- fit_Seedlings$draws(variables = "y_sim", format = "draws_matrix") %>% t()# matrix of observations simulated from the fitted model - row index for observations and colum index for simulations
+    y_hat <- fit_Seedlings$draws(variables = "y_hat", format = "draws_matrix") %>% apply(2, median, na.rm = T)
+    y <- data_seedlings$y
+    
+    residuals <- DHARMa::createDHARMa(simulatedResponse = y_sim, observedResponse = y, fittedPredictedResponse = y_hat, integerResponse = T)
+    
+    saveRDS(residuals, paste0(fitpath, "/Seedlings_", taxon, "_DHARMa", ".rds"))
+    
+    png(paste0(fitpath, "/Seedlings_", taxon, "_DHARMa", ".png"), width = 1600, height = 1000)
+    plot(residuals, quantreg = T, smoothScatter = F)
+    dev.off()
+    
+    return(NULL)
+  }
+  
+  sapply(names(fits_Seedlings), generateResiduals)
+
   return(fits_seedlings)
   
 }
-
