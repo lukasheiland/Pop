da0429f436f79871a578c1a10b6b7c1a3cf6dd38
Lukas <lukas.heiland@gmail.com>
2022-01-31 22:14:07 +0100

    Pretty plots

--- Theme/Theme.R
+++ Theme/Theme.R
@@ -1,21 +1,28 @@
 
 # Prerequisites -----------------------------------------------------------
 # remotes::install_version("Rttf2pt1", version = "1.3.8")
-library(showtext)
-font_add(family = "TGL17", regular = "Theme/tgl17.ttf")
-font_add(family = "SF Compact Rounded", regular = "Theme/SF-Compact-Rounded-Regular.otf") # bold = "Theme/SF-Compact-Rounded-Medium.otf", r
+# library(showtext)
+# font_add(family = "TGL17", regular = "Theme/tgl17.ttf")
+# font_add(family = "SF Compact Rounded", regular = "Theme/SF-Compact-Rounded-Regular.otf") # bold = "Theme/SF-Compact-Rounded-Medium.otf", r
 
 # Themes ------------------------------------------------------------------
 
 theme_fagus <- function(...) {
   theme_linedraw(...) +   # hrbrthemes::theme_ipsum(...) +
-    theme(axis.title.x = element_text(margin = margin(t = 3)), #add margin to x-axis title
+    theme(strip.background = element_rect_round(radius = unit(4, "pt")),
+          panel.background = element_rect_round(radius = unit(4, "pt")),
+          panel.border = element_rect_round(radius = unit(4, "pt"), size = rel(1.5)),
+          
+          panel.grid = element_line(colour = "#222222"), ## only slightly less weight than full black
+          axis.ticks = element_line(colour = "black", size = rel(0.8), lineend = "round"),
+          axis.title.x = element_text(margin = margin(t = 3)), #add margin to x-axis title
           axis.title.y = element_text(margin = margin(r = 2.5)),
           axis.text.x = element_text(margin = margin(t = 6)),
-          axis.text.y = element_text(margin = margin(r = 4.5)) ) +
+          axis.text.y = element_text(margin = margin(r = 4.5))
+          ) + 
+    
     theme(text = element_text(size = 14, family = "Helvetica")) # theme(text = element_text(size = 14, family = "SF Compact Rounded")) # theme(text = element_text(size = 14, family = "TGL17"))
   }
-
 
 theme_empty <- function(){
   theme(panel.grid = element_line(colour = "transparent"),
