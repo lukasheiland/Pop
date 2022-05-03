78bc465f1663d6e831340d409d6d587937662847
Lukas <code@heiland.earh>
2022-05-03 12:49:53 +0200

    Update Fit_functions.R

--- Fit_functions.R
+++ Fit_functions.R
@@ -274,9 +274,9 @@ formatStanData <- function(Stages, Stages_transitions, taxon_s, threshold_dbh, l
               
               ## Setting alpha = 1 for 0, so that the most density is towards zero
               # alpha = if_else(y_prior == 0, 1, 10),
-              alpha = case_when(stage == "J", 1 + 10 * count_obs, ## this will assign 1 to count_obs == 0
-                                stage == "A", 1 + 20 * count_obs,
-                                stage == "B", 1 + 1 * count_obs
+              alpha = case_when(stage == "J" ~ 1 + 10 * count_obs, ## this will assign 1 to count_obs == 0
+                                stage == "A" ~ 1 + 20 * count_obs,
+                                stage == "B" ~ 1 + 1 * count_obs
                                 ),
               alphaByE = alpha/y_prior_0,
               .groups = "drop")
